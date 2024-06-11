7af524814eb0149612407d2bdf3396880566a7fd
KyattPL <siema1234@gazeta.pl>
2024-06-11 15:14:27 +0200

    Correctly labeled and placed stuff

--- wyniki/analysis.py
+++ wyniki/analysis.py
@@ -52,44 +52,6 @@ mannwhitneyu_results_non_ai = [mannwhitneyu(
 mannwhitneyu_results_ai = [mannwhitneyu(
     ai_a[cols[0]], ai_b[cols[1]]) for cols in zip(ai_a.columns, ai_b.columns)]
 
-# ttest_results_a = ttest_rel(version_a.iloc[:, 7:26], version_a.iloc[:, 30:49])
-# ttest_results_b = ttest_rel(version_b.iloc[:, 7:26], version_b.iloc[:, 30:49])
-
-# Assuming ttest_results_a and ttest_results_b are Ttest_relResult objects
-# ttest_results_a_df = pd.DataFrame(ttest_results_a._asdict())
-# ttest_results_b_df = pd.DataFrame(ttest_results_b._asdict())
-
-# Save the results to CSV
-# ttest_results_a_df.to_csv('ttest_rel_version_A.csv')
-# ttest_results_b_df.to_csv('ttest_rel_version_B.csv')
-
-# Separate the responses for AI and non-AI from both versions
-# non_ai_a = version_a.iloc[:, 7:26].values.flatten()
-# ai_a = version_a.iloc[:, 30:49].values.flatten()
-# non_ai_b = version_b.iloc[:, 7:26].values.flatten()
-# ai_b = version_b.iloc[:, 30:49].values.flatten()
-
-# # Czy średnia dla wersji AI w przypadku formularza A i B jest taka sama
-# ind1_result = ttest_ind(ai_a, ai_b)
-
-# # Czy średnia dla wersji NONAI w przypadku formularza A i B jest taka sama``
-# ind2_result = ttest_ind(non_ai_a, non_ai_b)
-
-# # Wychodzą suuuper małe czyyyli że kolejność chyba ma znaczenie (?)
-# # W sensie średnia dla tej samej części ale z formularza A i B jest wyraźnie inna
-# print(ind1_result)
-# print(ind2_result)
-
-# Assuming ind1_result and ind2_result are Ttest_indResult objects
-# index_label = ['AI', 'Non-AI']
-# result_df = pd.DataFrame(
-#     {'AI': [ind1_result.pvalue], 'Non-AI': [ind2_result.pvalue]}, index=index_label)
-
-# Save the results to CSV
-# result_df.to_csv('ttest_ind_AI_NonAI.csv')
-
-# Create DataFrames for each set of results
-
 
 def create_test_results_df(test_results, test_name, columns):
     data = {
@@ -161,46 +123,18 @@ def create_test_results_df(test_results, test_name, columns):
 
 columns = non_ai_a.columns
 
-kruskal_results_non_ai = [kruskal(non_ai_a[cols[0]], non_ai_b[cols[1]])
-                          for cols in zip(non_ai_a.columns, non_ai_b.columns)]
-kruskal_results_ai = [kruskal(ai_a[cols[0]], ai_b[cols[1]])
-                      for cols in zip(ai_a.columns, ai_b.columns)]
+kruskal_results = []
+for cols in zip(non_ai_a.columns, ai_a.columns, non_ai_b.columns, ai_b.columns):
+    stat, p_value = kruskal(
+        non_ai_a[cols[0]], ai_a[cols[1]], non_ai_b[cols[2]], ai_b[cols[3]])
+    kruskal_results.append((cols[0], stat, p_value))
 
-# Kruskal-Wallis test results
-kruskal_non_ai_df = create_test_results_df(
-    kruskal_results_non_ai, 'Kruskal_Non_AI', columns)
-kruskal_ai_df = create_test_results_df(
-    kruskal_results_ai, 'Kruskal_AI', columns)
+kruskal_df = pd.DataFrame(kruskal_results, columns=[
+                          'Question', 'Kruskal_Statistic', 'p-value'])
 
 # Write all results to an Excel file
 with pd.ExcelWriter('non_parametric_tests_results.xlsx', engine='openpyxl') as writer:
-    kruskal_non_ai_df.to_excel(
-        writer, sheet_name='Kruskal_Non_AI_Results', index=False)
-    kruskal_ai_df.to_excel(
-        writer, sheet_name='Kruskal_AI_Results', index=False)
+    kruskal_df.to_excel(
+        writer, sheet_name='Kruskal_Results', index=False)
 
 print("Results saved to non_parametric_tests_results.xlsx")
-
-# only_quest_a = pd.read_excel('only_quest_a.xlsx')
-# only_quest_b = pd.read_excel('only_quest_b.xlsx')
-
-# # Create a DataFrame suitable for ANOVA
-# df_a = pd.melt(only_quest_a, id_vars=['Sygnatura czasowa'], value_vars=only_quest_a.columns[7:45],
-#                var_name='condition', value_name='score')
-# df_b = pd.melt(only_quest_b, id_vars=['Sygnatura czasowa'], value_vars=only_quest_b.columns[7:45],
-#                var_name='condition', value_name='score')
-
-# df_a['order'] = 'non-AI first'
-# df_b['order'] = 'AI first'
-
-# # Modify the 'Sygnatura czasowa' column to make subject IDs unique across groups
-# df_a['Sygnatura czasowa'] = df_a['Sygnatura czasowa'].astype(str) + '_A'
-# df_b['Sygnatura czasowa'] = df_b['Sygnatura czasowa'].astype(str) + '_B'
-
-# df = pd.concat([df_a, df_b])
-
-# # Mixed-Design ANOVA
-# aovrm = pg.mixed_anova(data=df, dv='score', subject='Sygnatura czasowa',
-#                        within='condition', between='order')
-
-# aovrm.to_csv('ANOVA_results.csv')
